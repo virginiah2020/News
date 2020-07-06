e09863cdd7c9d7a3ef28c203f6580faeb95e410e
virginiah2020 <vancyvy254@gmail.com>
2020-07-06 11:32:45 +0300

    def get_source on url

--- request.py
+++ request.py
@@ -20,3 +20,27 @@ def configure_request(app):
     # print(api_key)
     base_url = app.config['NEWS_API_BASE_URL']
     article_url = app.config['BASE_URL']
+
+def get_sources(category):
+    """
+    Function that gets the json response to our url request
+    """
+    get_sources_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        # print(get_sources_response)
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
+
+    return sources_results
+
+def process_results(sources_list):
+    """
+    Function that processes the sources result and tranforms them to a list of objects
+    """
