e3f033113f14ba6afbe84ecd9e177fdd445c70df
virginiah2020 <vancyvy254@gmail.com>
2020-07-06 11:34:03 +0300

    a return function on source

--- request.py
+++ request.py
@@ -44,3 +44,17 @@ def process_results(sources_list):
     """
     Function that processes the sources result and tranforms them to a list of objects
     """
+    
+    sources_results = []
+    for sources_item in sources_list:
+        id = sources_item.get('id')
+        name = sources_item.get('name')
+        description = sources_item.get('description')
+        url = sources_item.get('url')
+        category = sources_item.get('category')
+        language = sources_item.get('language')
+        country = sources_item.get('country')
+        sources_object = Sources(id, name, description, url, category, country, language)
+        sources_results.append(sources_object)
+
+    return sources_results
